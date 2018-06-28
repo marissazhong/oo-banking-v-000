ab66faa42cbfd4a0b140349333c3873546f760e4
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 23:57:00 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,7 +4,7 @@ class Transfer
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
-    @amount = transfer_amount
+    @amount = amount
     @status = 'pending'
   end
 
