7725654f4bc60e0f5ea5e4e7bc61df0652ddd34c
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 23:55:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,17 @@
 class Transfer
   attr_accessor :sender, :receiver, :transfer_amount, :status
+  
   def initialize(sender, receiver, transfer_amount)
     @sender = sender
     @receiver = receiver
     @transfer_amount = transfer_amount
     @status = 'pending'
   end
+
+  def valid?
+
+  end
+
+  
 
 end
