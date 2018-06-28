46da88aeff9ae0bb1bab31b294fe7b5238f52ee5
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 23:55:46 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
   attr_accessor :sender, :receiver, :transfer_amount, :status
-  
+
   def initialize(sender, receiver, transfer_amount)
     @sender = sender
     @receiver = receiver
@@ -12,6 +12,8 @@ class Transfer
 
   end
 
-  
+  def execute_transaction
+
+  end
 
 end
