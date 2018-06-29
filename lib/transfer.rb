cb7d7d35663f76781d9d28f2f9f9548fb4ee84ae
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 00:02:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,32 @@ class Transfer
 
 end
 
+class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == 'open' && @balance > 0
+  end
+
+  def close_account
+    @status = 'closed'
+  end
+end
+
 let(:avi) { BankAccount.new("Avi") }
 let(:amanda) { BankAccount.new("Amanda") }
 let(:transfer) { Transfer.new(amanda, avi, 50) }
